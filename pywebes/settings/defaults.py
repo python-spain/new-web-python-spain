d63f693ca25b94a7500586f063d48bed929d1006
Nekmo <contacto@nekmo.com>
2016-07-03 19:10:57 +0200

    Removed STATICFILES_DIRS from settings.

--- pywebes/settings/defaults.py
+++ pywebes/settings/defaults.py
@@ -65,9 +65,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'pywebes', 'static'),
-)
 SITE_ID = 1
 
 
