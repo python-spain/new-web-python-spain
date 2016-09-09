fb15fcd707c4b6c47a46811f0c4411b9d91c06f7
Nekmo <contacto@nekmo.com>
2016-09-09 16:34:53 +0200

    Sqlite settings file.

---
+++ pywebes/settings/sqlite.py
@@ -0,0 +1,13 @@
+from .develop import *
+
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
